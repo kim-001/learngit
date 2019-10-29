64ca2e0feeebb00bb75db262f8632c0a328cb13f
kimball <jb05-1@163.com>
2019-10-29 14:22:39 +0800

    fist commit

---
+++ learn_git/learn_git/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  learn_git
+//
+//  Created by 姜波 on 2019/10/29.
+//  Copyright © 2019年 姜波. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
